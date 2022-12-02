08b60d3b78cc95aeae194e14d7504066f93f8898
natsuki <ashi2727n@gmail.com>
2022-12-02 23:28:43 +0900

    update Assignment3

--- Assignment3/app/go/main.go
+++ Assignment3/app/go/main.go
@@ -1,51 +1,58 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
 
-    // postgres ドライバ
-    _ "github.com/lib/pq"
+	// postgres ドライバ
+	_ "github.com/lib/pq"
 )
 
 // TestUser : テーブルデータ
 type TestUser struct {
-    UserID   int
-    Password string
+	UserID   int
+	Password string
 }
 
-// メイン関数
+// main 関数
 func main() {
+	http.HandleFunc("/a", handler)
+	http.ListenAndServe(":8181", nil)
+}
 
-    // Db: データベースに接続するためのハンドラ
-    var Db *sql.DB
-    // Dbの初期化
-    Db, err := sql.Open("postgres", "host=postgres user=app_user password=password dbname=app_db sslmode=disable")
-    if err != nil {
-        log.Fatal(err)
-    }
+// handler 関数
+func handler(w http.ResponseWriter, r *http.Request) {
 
-    // SQL文の構築
-    sql := "SELECT user_id, user_password FROM TEST_USER WHERE user_id=$1;"
+	// Db: データベースに接続するためのハンドラ
+	var Db *sql.DB
+	// Dbの初期化
+	Db, err := sql.Open("postgres", "host=postgres user=app_user password=password dbname=app_db sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // preparedstatement の生成
-    pstatement, err := Db.Prepare(sql)
-    if err != nil {
-        log.Fatal(err)
-    }
+	// SQL文の構築
+	sql := "SELECT user_id, user_password FROM TEST_USER WHERE user_id=$1;"
 
-    // 検索パラメータ（ユーザID）
-    queryID := 1
-    // 検索結果格納用の TestUser
-    var testUser TestUser
+	// preparedstatement の生成
+	pstatement, err := Db.Prepare(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // queryID を埋め込み SQL の実行、検索結果1件の取得
-    err = pstatement.QueryRow(queryID).Scan(&testUser.UserID, &testUser.Password)
-    if err != nil {
-        log.Fatal(err)
-    }
+	// 検索パラメータ（ユーザID）
+	queryID := 1
+	// 検索結果格納用の TestUser
+	var testUser TestUser
 
-    // 検索結果の表示
-    fmt.Println(testUser.UserID, testUser.Password)
+	// queryID を埋め込み SQL の実行、検索結果1件の取得
+	err = pstatement.QueryRow(queryID).Scan(&testUser.UserID, &testUser.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 検索結果の表示
+	fmt.Fprintf(w, testUser.Password)
 }
